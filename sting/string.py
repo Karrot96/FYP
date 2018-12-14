a15a0d1aa7541a86440132e86843811b8f70aa2b
Karrot96-D <apk115@ic.ac.uk>
2018-12-14 20:07:22 +0000

    fs

---
+++ sting/string.py
@@ -0,0 +1,16 @@
+# import numpy as np
+
+# class Rope:
+#     NODES = 100
+#     string = np.zeros((NODES, 3))
+#
+#     def __init__(self):
+#         for i in range(0,len(string)):
+#             self.string[i] = np.array(i,i,i)
+#         print(self.string)
+print("hello")
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+print('how do I make a game?')
