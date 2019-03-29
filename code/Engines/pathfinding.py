b820d03bce7af0dae93d0c6b9b16765681180324
Karrot96-D <apk115@ic.ac.uk>
2019-03-29 22:01:33 +0000

    comments

--- code/Engines/pathfinding.py
+++ code/Engines/pathfinding.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import logging as log
 np.set_printoptions(threshold=sys.maxsize)
 
 
@@ -12,17 +13,26 @@ class Paths:
     totalDistance=-1
     best = None
     def __init__(self, points):
+        """Used to hande the finding of the shortest path to connectg a set of points - definitely not efficient
+    
+        Variabels:
+            points {2-D numpy array} - list of points to be connected
+        """
         self.points = points
-    def plot(self):
-        x=[]
-        y=[]
-        for i in range(0,len(self.points)):
-            x.append(self.best[i,0][0])
-            y.append(self.best[i,0][1])
-        plt.plot(x,y)
-        plt.show()
 
     def reorder(self, pointsRemaining, start, new, first):
+        """ Reorders the array to that of the shortest distanace between points given a certain starting point
+        
+        Arguments:
+            pointsRemaining {2-D array} -- Array of the points yet to be sorted
+            start {1-D array of shape (2)} -- Starting point for the sort
+            new {None} -- Used in the recursion
+            first {Bool} -- Used to determine if on first level of recursion
+        
+        Returns:
+            int -- always 0
+        """
+
         if first:
             distances = np.linalg.norm(pointsRemaining-start, axis=1)
             x=np.argmin(distances)
@@ -47,6 +57,12 @@ class Paths:
                 self.totalDistance=np.sum(values)
         return 0
     def iterate(self):
+        """Used to interate through all possible lists to find shortest distance
+
+            Returns:
+                {(list[float],list[float])} - (x,y) points
+        """
+
         for i in range(0,len(self.points)):
             self.reorder(self.points, self.points[0],None, True)
             self.points = np.roll(self.points,1)
