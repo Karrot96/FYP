4d2243c78bcad417c79185715572ab625e66edaf
Karrot96-L <apk115@ic.ac.uk>
2019-05-15 17:23:52 +0100

    mask based

--- code/Engines/mask_mapping.py
+++ code/Engines/mask_mapping.py
@@ -11,6 +11,117 @@ from Engines.path_finding import Paths
 np.set_printoptions(threshold=sys.maxsize)
 
 
+class Path:
+    """
+    """
+
+    def __init__(self, points, mask):
+        """[summary]
+        Arguments:
+            points {[type]} -- (y,x)
+            mask {[type]} -- [description]
+        """
+        self.MASK_WEIGHT = 5
+        self.points = points
+        self.mask = mask
+
+    def flip_x(self, x1, x2, mask):
+        if x1 > x2:
+            return np.flip(mask, axis=1)
+        return mask
+
+    def flip_y(self, y1, y2, mask):
+        if y1 > y2:
+            return np.flip(mask, axis=0)
+        return mask
+
+    def mask_distance(self, p1, p2):
+        """[summary]
+
+        Arguments:
+            p1 {[type]} -- [description]
+            p2 {[type]} -- [description]
+        """
+        if p1 == p2:
+            return (-255*4000)
+        x_diff = abs(p1[1]-p2[1])
+        y_diff = abs(p1[0]-p2[0])
+        if y_diff > x_diff:
+            mask = mask.T
+            x1 = p1[0]
+            y1 = p1[1]
+            x2 = p2[0]
+            y2 = p2[1]
+            x_diff = abs(p1[0]-p2[0])
+            y_diff = abs(p1[1]-p2[1])
+        else:
+            y1 = p1[0]
+            x1 = p1[1]
+            y2 = p2[0]
+            x2 = p2[1]
+        mask = flip_x(x1, x2, flip_y(y1, y2, np.copy(self.mask)))
+        x_len = len(mask[0])
+        mask_flat = np.ravel(mask)
+        total = 0
+        for i in range(0, round(x_diff/y_diff)):
+            values = mask_flat[y1*x_len+x1+i:y2*x_len+1+i:x_len+1]
+            log.debug("Mask Values on iter %s: \n %s", i, values)
+            total += np.sum(values)
+        return total
+
+    def algorithm(self, start, new, search_space):
+        if search_space.any():
+            distances = np.linalg.norm(search_space-start, axis=1)
+            mask_values = np.apply_along_axis(
+                self.mask_distance,
+                1,
+                search_space,
+                start
+            )
+            # Mask returns 255 when detected space exsists
+            # Inverting so returns 0 when line is all in mask
+            # Adjusting to resonable scale
+            mask_distance = abs(mask_values - 255)/255
+            # Assigning a weight based on how far apart the points
+            # are and the time spend outside mask
+            total_weighted_distance = distances+self.MASK_WEIGHT*mask_distance
+            index = np.argmin(total_weighted_distance)
+            new = np.append(
+                new,
+                [[search_space[index], total_weighted_distance[index]]],
+                axis=0
+            )
+            to_return = self.algorithm(
+                search_space[index],
+                new,
+                np.delete(search_space, index, 0)
+            )
+            return to_return
+        else:
+            return (np.sum([:, 1]), new)
+
+    def iterate(self):
+        possibile_paths = [None for _ in range(len(self.points))]
+        path_lengths = np.copy(possibile_paths)
+        for i, point in enumerate(self.points):
+            new = np.array([[self.points, 0]])
+            path_lengths[i], possibile_paths[i] = self.algorithm(
+                point,
+                new,
+                np.delete(self.points, i, 0)
+            )
+        index = np.argmin(path_lengths)
+        x_values = []
+        y_values = []
+        # TODO This should be able to be done using np.apply_along_axis for
+        # speed up
+        for best in possibile_paths[index]:
+            x_values.append(best[0])
+            y_values.append(best[1])
+        return (x_values, y_values)
+        # np.apply_along_axis(self.algorithm, 1, new, [])
+
+
 class Engine:
     """[summary]
     """
