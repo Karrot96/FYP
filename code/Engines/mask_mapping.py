375f2c435edccde2500f79a731753ce2e93be075
Karrot96-L <apk115@ic.ac.uk>
2019-05-15 14:05:02 +0100

    bath processing testing

--- code/Engines/mask_mapping.py
+++ code/Engines/mask_mapping.py
@@ -6,6 +6,8 @@ import logging as log
 import numpy as np
 import rope
 from sklearn.cluster import MiniBatchKMeans
+from scipy import spatial
+from Engines.path_finding import Paths
 np.set_printoptions(threshold=sys.maxsize)
 
 class Engine:
@@ -28,7 +30,7 @@ class Engine:
             int -- index of the closest point within plot
         """
         # ! kmeans++ should probably be changed to the rope.lace
-        locations = KMeans(
+        locations = MiniBatchKMeans(
             n_clusters=self.rope.NO_NODES,
             init='k-means++',
             batch_size=10,
@@ -37,12 +39,139 @@ class Engine:
         log.debug("Locations are: \n %s", locations)
         return locations
     
-    def run(self, edges, mask):
-        """[summary]
+    # def run(self, edges, mask):
+    #     """[summary]
         
+    #     Arguments:
+    #         edges {[type]} -- [description]
+    #         mask {[type]} -- [description]
+    #     """
+    #     clusters = self.kmeans(mask)
+
+    def nearestneighbours(self, plot, points, k):
+        """Find the nearest point to another from a point map
+
         Arguments:
-            edges {[type]} -- [description]
-            mask {[type]} -- [description]
+            plot {2-D np.array} -- A collection of 2-D locations of all
+                                   the points
+            points {tuple} -- (x,y) of the point to be found within the
+                              plot
+            k {int} -- The number of the closest point to be returned
+
+        Returns:
+            int -- index of the closest point within plot
         """
-        clusters = self.kmeans(mask)
+
+        tree = spatial.cKDTree(plot)
+        _, indexes1 = tree.query(points, k=[k])
+        return indexes1
+
+    def adjusted_mean(self, arr):
+        """Checks to make sure points are close enough together to be
+        considered parts of a string
+
+        Arguments:
+            arr {np.array} -- [2 points in an array to have mean found of]
+
+        Returns:
+            [float] -- [The mean of 2 points]
+        """
+
+        if arr[1]-arr[0] < 100:
+            return np.mean(arr)
+        else:
+            return arr[0]
+
+    def get_points(self, arr):
+        """Returns the points of the shoelace by looking for edges
+
+        Arguments:
+            a {np.array} -- 1-D array of all points on a given pixel line
+
+        Returns:
+            np.array -- all non-zero points on a line
+        """
+        non_zero = np.nonzero(arr)  # Get non-zero y values constituting edges
+        first_non_zero = non_zero[0]
+        if first_non_zero.any():
+            log.debug("A:%s B:%s", len(arr), len(first_non_zero))
+            log.debug("Z:%s", first_non_zero)
+        # Output array same size as input array so numpy doesnt complain
+        out = np.pad(
+            first_non_zero,
+            (0, len(arr)-len(first_non_zero)),
+            'constant',
+            constant_values=(0, 0)
+            )
+        return out  # Return the midpoints of each point
+
+    def locate_y(self, arr):
+        """Find the y value associated with nonzero value a
+
+        Arguments:
+            a {np.array} -- [x y] of the index of the needed y value within
+                         self.lace
+
+        Returns:
+            [tuple] -- (x,y) of the location within frame of the point
+        """
+
+        y_location = self.lace[arr[0], arr[1]]
+        return (arr[1], y_location)
+
+    def locate_x(self, arr):
+        """Find the x value associated with nonzero value a
+
+        Arguments:
+            a {np.array} -- [x y] of the index of the needed x value within
+                         self.lace
+
+        Returns:
+            [tuple] -- (x,y) of the location within frame of the point
+        """
+        x_location = self.lace[arr[0], arr[1]]
+        return (x_location, arr[0])
+
+    def run(self, edges):
+        """ Used to run the processing on images
+
+        Arguments:
+            edges {np.array} -- Image in a greyscale format
+
+        Returns:
+            Rope -- full upadated rope obkect
+        """
+
+        self.lace = np.apply_along_axis(self.get_points, 0, edges)
+        shoelace = np.nonzero(self.lace)  # Remove padded 0's
+        combined_y = np.apply_along_axis(
+            self.locate_y,
+            1,
+            np.transpose(shoelace)
+            )
+        self.lace = np.apply_along_axis(self.get_points, 1, edges)
+        # * Transpose lace so that non-zero acts on the right axis.
+        # * Transpose shoelace so that the points are outputs in array of [y x]
+        shoelace = np.nonzero(self.lace)
+        combined_x = np.apply_along_axis(
+            self.locate_x,
+            1,
+            np.transpose(shoelace)
+            )
+        combined = np.concatenate((combined_x, combined_y))
+        clusters = self.kmeans(combined[0::20])
+        log.debug("clusters: \n %s", clusters)
+        log.debug(np.shape(clusters))
+        path = Paths(clusters)
+        x_locations, y_locations = path.iterate()
+        log.debug("x: %s", x_locations)
+        log.debug("y: %s", y_locations)
+        for i, (j, k) in enumerate(zip(x_locations, y_locations)):
+            self.rope.lace[i] = np.array([
+                int(j),
+                int(k),
+                self.rope.lace[i][2]
+                ])
+        log.debug("rope: %s", self.rope.lace)
+        return self.rope
     
